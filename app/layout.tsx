1464f8188aefc1af78fd4707bf98b8b0d719bdff
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-08-22 17:16:07 +0900

    feat: 초기파일추가 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
-};
-
-export default RootLayout;
+}
