57f1a05311c3af33c7cb44427b3a9055457e34d1
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-08-22 17:14:58 +0900

    feat: 초기파일추가 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Hello World!</div>;
-}
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app using the app directory!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
